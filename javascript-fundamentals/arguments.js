8acc9bd0a0e7d567777f7ff372fd8b1a3c4aa249
Arthur Faria <arthur.faria@getpingback.com>
2023-10-26 16:59:08 -0300

    arguments and rest parameter

---
+++ javascript-fundamentals/arguments.js
@@ -0,0 +1,7 @@
+// rest parameter must be the last parameter
+function sum (discount, ...prices){
+    const total = prices.reduce((a, b) => a + b);
+    return total * (1 - discount);
+}
+
+console.log(sum(0.1, 20, 30, 50));
