c4cd496acd20c7f7b4d59c855c602ad5f63010b7
Arthur Faria <arthur.faria@getpingback.com>
2023-10-26 17:20:29 -0300

    try and catch

--- javascript-fundamentals/gettersAndSetters.js
+++ javascript-fundamentals/gettersAndSetters.js
@@ -8,11 +8,27 @@ const person = {
         return `${person.firstName} ${person.lastName}`
     },
     set fullName(value) {
+        
+        if(typeof value !== 'string')
+            throw new Error('Value is not a string');
+
         const parts = value.split(' ');
+
+        if(parts.length !== 2)
+            throw new Error('Enter a first and last name');
+
         this.firstName = parts[0];
         this.lastName = parts[1];
     }
 }
-person.fullName = 'A F';
-console.log(person.fullName);
-console.log(person);
+
+// person.fullName = 'A F';
+// console.log(person.fullName);
+// console.log(person);
+
+
+try {
+    person.fullName = '';
+}catch(e){
+    console.log(e);
+}
