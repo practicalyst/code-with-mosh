ad51a0fb182af71ff161707b976fd52c7d74136d
Arthur Faria <arthur.faria@getpingback.com>
2023-11-17 11:23:05 -0300

    fixing sendEmails

--- node/async-demo/exercise.js
+++ node/async-demo/exercise.js
@@ -11,35 +11,38 @@
 // });
 
 async function displayInfo() {
-    const customer = await getCustomer(1);
-    console.log('Customer: ', customer);
-    if (customer.isGold) {
-      const movies = await getTopMovies();
-      console.log("Top movies: ", movies);
-      sendEmail(customer.email, movies);
-      console.log("Email sent...");
-    }
+  const customer = await getCustomer(1);
+  console.log("Customer: ", customer);
+  if (customer.isGold) {
+    const movies = await getTopMovies();
+    console.log("Top movies: ", movies);
+    await sendEmail(customer.email, movies);
+    console.log("Email sent...");
   }
-  
-  displayInfo();
-  
-  function getCustomer(id) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({ id, name: "AF", isGold: true, email: "af@af.com" });
-      }, 4000);
-    });
-  }
-  
-  function getTopMovies() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(["movie1", "movie2"]);
-      }, 4000);
-    });
-  }
-  
-  function sendEmail(email, movies) {
-    setTimeout(() => {}, 4000);
-  }
-  
+}
+
+displayInfo();
+
+function getCustomer(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ id, name: "AF", isGold: true, email: "af@af.com" });
+    }, 4000);
+  });
+}
+
+function getTopMovies() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(["movie1", "movie2"]);
+    }, 4000);
+  });
+}
+
+function sendEmail(email, movies) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
+}
